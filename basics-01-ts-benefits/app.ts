41ca2b49698ebcdc3426b1c9376b2f87032e01aa
sorrel012 <wain1719@gmail.com>
2024-02-26 17:02:27 +0900

    feat: Explore typescript types

--- basics-01-ts-benefits/app.ts
+++ basics-01-ts-benefits/app.ts
@@ -1,14 +1,20 @@
-function addNumbers(n1: number, n2: number, showResult: boolean) {
-  if(showResult) {
-    console.log(n1 + n2);
+function addNumbers(
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  phrase: string,
+) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
   } else {
-    return n1 + n2;
+    return result;
   }
-
 }
 
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = 'Result is: ';
 
-addNumbers(number1, number2, printResult);
+addNumbers(number1, number2, printResult, resultPhrase);
