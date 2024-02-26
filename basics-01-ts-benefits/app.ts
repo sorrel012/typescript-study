21bbe9759cc6f24ea0ea2d486ad0839189fe2342
sorrel012 <wain1719@gmail.com>
2024-02-26 16:27:48 +0900

    feat: Explore boolean type

--- basics-01-ts-benefits/app.ts
+++ basics-01-ts-benefits/app.ts
@@ -1,9 +1,14 @@
-function addNumbers(n1: number, n2: number) {
-  return n1 + n2;
+function addNumbers(n1: number, n2: number, showResult: boolean) {
+  if(showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
+
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = addNumbers(number1, number2);
-console.log(result);
+addNumbers(number1, number2, printResult);
