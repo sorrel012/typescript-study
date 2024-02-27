1f7efa7d096381cd0b8c2699c0ac9612492f2fad
sorrel012 <wain1719@gmail.com>
2024-02-27 14:48:35 +0900

    feat: Explore literal type

--- basics-01-ts-types/app.ts
+++ basics-01-ts-types/app.ts
@@ -1,12 +1,21 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: string,
+) {
   let result;
   if (typeof input1 === 'number' && typeof input2 === 'number') {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+  if (resultConversion === 'as-number') {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combinedAges = combine(30, 26);
-const combinedNames = combine('Hana', 'Han');
+const combinedAges = combine(30, 26, 'as-number');
+const combinedStringAges = combine('30', '26', 'as-number');
+const combinedNames = combine('Hana', 'Han', 'as-text');
