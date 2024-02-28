1618e3c547f83d12b2b8feedbd1a68351c759b39
sorrel012 <wain1719@gmail.com>
2024-02-28 14:11:26 +0900

    feat: Explore spread syntax

--- 03-next-gen/src/app.js
+++ 03-next-gen/src/app.js
@@ -1,13 +1,11 @@
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
-
 activeHobbies.push(...hobbies);
 console.log(activeHobbies);
-
 const person = {
-  name: 'Hana',
-  age: 27,
+    name: 'Hana',
+    age: 27,
 };
-
-const copiedPerson = { ...person };
+const copiedPerson = Object.assign({}, person);
 console.log(copiedPerson);
+//# sourceMappingURL=app.js.map
