043d2eed73e2ae65ec96cc287e5d5be082bc5bd6
sorrel012 <wain1719@gmail.com>
2024-02-28 16:24:49 +0900

    feat: Explore typescript class

---
+++ 04-class-interfaces/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+new Department('Accounting');
+//# sourceMappingURL=app.js.map
--- 04-class-interfaces/src/app.ts
+++ 04-class-interfaces/src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+new Department('Accounting');
