7cf7ce8f9e97120ec86ea176e92eef2f99fd7140
sorrel012 <wain1719@gmail.com>
2024-02-28 14:15:27 +0900

    feat: Explore rest parameter

--- 03-next-gen/src/app.js
+++ 03-next-gen/src/app.js
@@ -1,11 +1,18 @@
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
 activeHobbies.push(...hobbies);
-console.log(activeHobbies);
+// console.log(activeHobbies);
 const person = {
     name: 'Hana',
     age: 27,
 };
 const copiedPerson = Object.assign({}, person);
-console.log(copiedPerson);
+// console.log(copiedPerson);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 20);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
--- 03-next-gen/src/app.ts
+++ 03-next-gen/src/app.ts
@@ -2,7 +2,7 @@ const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
 
 activeHobbies.push(...hobbies);
-console.log(activeHobbies);
+// console.log(activeHobbies);
 
 const person = {
   name: 'Hana',
@@ -10,4 +10,13 @@ const person = {
 };
 
 const copiedPerson = { ...person };
-console.log(copiedPerson);
+// console.log(copiedPerson);
+
+const add = (...numbers: [number, number, number]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 20);
+console.log(addedNumbers);
