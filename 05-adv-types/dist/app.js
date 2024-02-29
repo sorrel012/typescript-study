4115b452e8e640cdbd75582afcb9d67610f28a4f
sorrel012 <wain1719@gmail.com>
2024-02-29 15:15:33 +0900

    feat: Explore typescript type guard

--- 05-adv-types/dist/app.js
+++ 05-adv-types/dist/app.js
@@ -21,4 +21,27 @@ function printEmployeeInformation(emp) {
     }
 }
 printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo... ' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- 05-adv-types/src/app.ts
+++ 05-adv-types/src/app.ts
@@ -42,3 +42,34 @@ function printEmployeeInformation(emp: UnknownEmployee) {
 }
 
 printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo... ' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
