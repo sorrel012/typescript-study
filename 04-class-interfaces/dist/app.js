0509d98ae8a83fc3ef777d42ddc60126755b7ecc
sorrel012 <wain1719@gmail.com>
2024-02-29 10:59:47 +0900

    feat: Explore typescript interface

--- 04-class-interfaces/dist/app.js
+++ 04-class-interfaces/dist/app.js
@@ -1,11 +1,14 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Hana',
-    age: 27,
+class Person {
+    constructor(n) {
+        this.age = 27;
+        this.name = n;
+    }
     greet(phrase) {
-        console.log(`${phrase} I'm ${this.name}`);
-    },
-};
-user1.greet('Hi- there,');
+        console.log(`${phrase} I'm ${this.name}, and I'm ${this.age} years old.`);
+    }
+}
+let user1;
+user1 = new Person('Hana');
+user1.greet('Hi~~');
 //# sourceMappingURL=app.js.map
--- 04-class-interfaces/src/app.ts
+++ 04-class-interfaces/src/app.ts
@@ -1,18 +1,33 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age: number = 27;
 
-user1 = {
-  name: 'Hana',
-  age: 27,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
-    console.log(`${phrase} I'm ${this.name}`);
-  },
-};
+    console.log(`${phrase} I'm ${this.name}, and I'm ${this.age} years old.`);
+  }
+}
 
-user1.greet('Hi- there,');
+let user1: Greetable;
+
+user1 = new Person('Hana');
+user1.greet('Hi~~');
+
+// user1 = {
+//   name: 'Hana',
+//   age: 27,
+//   greet(phrase: string) {
+//     console.log(`${phrase} I'm ${this.name}`);
+//   },
+// };
+//
+// user1.greet('Hi- there,');
